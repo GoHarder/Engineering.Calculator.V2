8cb5a1c727c608d730ce9d53b65a7153abcc2070
Gregory Harder <gharder@hwec.com>
2021-11-10 12:51:54 -0600

    Installed rollup

---
+++ rollup.config.js
@@ -0,0 +1,49 @@
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+import rootImport from 'rollup-plugin-root-import';
+import livereload from 'rollup-plugin-livereload';
+import scss from 'rollup-plugin-scss';
+import svelte from 'rollup-plugin-svelte';
+import sveltePre from 'svelte-preprocess';
+import svg from 'rollup-plugin-svg';
+import { terser } from 'rollup-plugin-terser';
+
+const production = !process.env.ROLLUP_WATCH;
+
+const svelteConfig = {
+   preprocess: sveltePre(),
+   compilerOptions: {
+      dev: !production,
+   },
+};
+
+const appConfig = {
+   input: 'src/js/app.js',
+   output: {
+      sourcemap: true,
+      format: 'iife',
+      name: 'app',
+      file: 'public/app.js',
+   },
+   plugins: [
+      rootImport({
+         root: __dirname,
+         useInput: 'prepend',
+         extensions: '.js',
+      }),
+      svelte(svelteConfig),
+      scss({ output: 'public/style.css' }),
+      json(),
+      svg(),
+      resolve({ browser: true, dedupe: ['svelte'] }),
+      commonjs(),
+      !production && livereload('public'),
+      production && terser(),
+   ],
+   watch: {
+      clearScreen: false,
+   },
+};
+
+export default [appConfig];
