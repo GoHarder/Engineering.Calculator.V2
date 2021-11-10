63a39c0b21cd9dbd125387ce8bc7fa8fd35ecdfd
Gregory Harder <gharder@hwec.com>
2021-11-10 09:01:31 -0600

    Initial commit

---
+++ server/env/env.mjs
@@ -0,0 +1,65 @@
+// Node Imports
+import { readFileSync } from 'fs';
+
+// Project Imports
+import { toCamelCase } from '../../lib/string.mjs';
+
+/** The server environment */
+let env = undefined;
+
+/**
+ * Converts a string to a JSON value
+ * @param str The string to convert
+ */
+const parseValue = (str) => {
+   try {
+      return JSON.parse(str);
+   } catch (error) {
+      return str;
+   }
+};
+
+/** Gets the data from the environment file */
+export const getEnv = () => {
+   if (env) return env;
+
+   try {
+      let path = new URL('./.env', import.meta.url);
+      let fileData = readFileSync(path, 'utf8');
+
+      // Split string into array
+      fileData = fileData.split('\n');
+
+      // Remove \r at the end of each line
+      fileData = fileData.map((line) => line.replace('\r', ''));
+
+      // Remove empty lines
+      fileData = fileData.filter((line) => line);
+
+      // Remove comments
+      fileData = fileData.filter((line) => !line.match(/^#/));
+
+      // Create obj and set the env variable and return the data
+      fileData = fileData.reduce((obj, line) => {
+         let key = line.replace(/(^\w+)=.+/g, '$1').toLowerCase();
+         key = toCamelCase(key.replace(/_/g, ' '));
+
+         let value = line.replace(/^\w+=(.+)/g, '$1');
+
+         obj[key] = parseValue(value);
+
+         return obj;
+      }, {});
+
+      env = fileData;
+
+      process.env.PORT = env.port;
+
+      return fileData;
+   } catch (error) {
+      console.log(error);
+      return {};
+   }
+};
+
+console.log(getEnv());
