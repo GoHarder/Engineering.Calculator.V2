4327c836ae5e24d0534e96ef53432654c11cab04
Gregory Harder <gharder@hwec.com>
2021-11-11 10:24:56 -0600

    Added icons

---
+++ public/img/icons/index.js
@@ -0,0 +1,5 @@
+import link from './link.svg';
+import visibility_off from './visibility_off.svg';
+import visibility from './visibility.svg';
+
+export { link, visibility_off, visibility };
--- public/img/index.js
+++
@@ -1,3 +0,0 @@
-import link from './link.svg';
-
-export { link };
