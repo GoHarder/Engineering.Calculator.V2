bca43e66769ffa25320d4e014690378e7f981fe6
Gregory Harder <gharder@hwec.com>
2021-11-12 09:09:02 -0600

    Added forward events to component library

--- src/svelte/components/lib.js
+++ src/svelte/components/lib.js
@@ -1,3 +1,90 @@
+/**
+ * @module lib Library of functions to create components
+ */
+
+import { bubble, listen } from 'svelte/internal';
+
+/**
+ * Adds custom events to a component
+ * @param {object} component The component object
+ * @param {object} addedEvents Custom events to include
+ */
+export const forwardEvents = (component, addedEvents = []) => {
+   const events = [
+      'focus',
+      'blur',
+      'fullscreenchange',
+      'fullscreenerror',
+      'scroll',
+      'cut',
+      'copy',
+      'paste',
+      'keydown',
+      'keypress',
+      'keyup',
+      'auxclick',
+      'click',
+      'contextmenu',
+      'dblclick',
+      'mousedown',
+      'mouseenter',
+      'mouseleave',
+      'mousemove',
+      'mouseover',
+      'mouseout',
+      'mouseup',
+      'pointerlockchange',
+      'pointerlockerror',
+      'select',
+      'wheel',
+      'drag',
+      'dragend',
+      'dragenter',
+      'dragstart',
+      'dragleave',
+      'dragover',
+      'drop',
+      'touchcancel',
+      'touchend',
+      'touchmove',
+      'touchstart',
+      'pointerover',
+      'pointerenter',
+      'pointerdown',
+      'pointermove',
+      'pointerup',
+      'pointercancel',
+      'pointerout',
+      'pointerleave',
+      'gotpointercapture',
+      'lostpointercapture',
+      ...addedEvents,
+   ];
+
+   const forward = (event) => bubble(component, event);
+
+   return (node) => {
+      const destructors = [];
+
+      for (let i = 0; i < events.length; i++) {
+         destructors.push(listen(node, events[i], forward));
+      }
+
+      return {
+         destroy: () => {
+            for (let i = 0; i < destructors.length; i++) {
+               destructors[i]();
+            }
+         },
+      };
+   };
+};
+
+/**
+ * Filters a components properties to pass down to other components
+ * @param {object} props The components $$props object
+ * @param {[string]} filter An array of properties to filter
+ */
 export const filterProps = (props, filter) => {
    return ['$$slots', '$$scope', ...filter].reduce(
       (obj, filterItem) => {
