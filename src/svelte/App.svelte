cc675fb217a270e6bf2e0863fd3deba9e8cbb38a
Gregory Harder <gharder@hwec.com>
2021-11-12 11:03:10 -0600

    Added pages

--- src/svelte/App.svelte
+++ src/svelte/App.svelte
@@ -1,27 +1,50 @@
 <script>
-   import { onDestroy } from 'svelte';
+   import { onDestroy, onMount } from 'svelte';
 
    // Components
    import { Button } from './components/material/button';
    import { Dialog, Title } from './components/material/dialog';
    import { LinearProgress } from './components/material/progress';
 
+   import Home from './pages/home/Home.svelte';
+   import Login from './pages/login/Login.svelte';
+
    // Stores
    import fetchStore from './stores/fetch';
+   import userStore from './stores/user';
 
    // Properties
    // Methods
    // Constants
+   const comps = {
+      Home,
+      Login,
+   };
+
    // Variables
+   let comp = Login;
    let errorData = undefined;
+   let showApp = false;
    let showError = false;
    let showLoading = false;
+   let user = undefined;
 
    // Subscriptions
-   const clearFetch = fetchStore.subscribe((store) => (showLoading = store.loading));
+   const clearFetch = fetchStore.subscribe((store) => {
+      errorData = store.errorData;
+      showLoading = store.loading;
+   });
+
+   const clearUser = userStore.subscribe((store) => (user = store));
 
    // Contexts
    // Reactive Rules
+   $: if (errorData) showError = true;
+
+   $: if (user && comp === Login) history.pushState({ path: '/Home' }, '');
+
+   $: if (!user) history.pushState({ path: '/Login' }, '');
+
    // Events
    const onBeforeUnload = () => {
       console.log('before unload');
@@ -30,12 +53,19 @@
    const onCloseError = () => fetchStore.clearError();
 
    const onLocationChange = () => {
-      console.log('location change');
+      const path = history.state.path.split('/').slice(1);
+      comp = comps[path[0]];
    };
 
    // Lifecycle
+   onMount(() => {
+      history.pushState({ path: '/Login' }, '');
+      setTimeout(() => (showApp = true), 1000);
+   });
+
    onDestroy(() => {
       clearFetch();
+      clearUser();
    });
 </script>
 
@@ -56,6 +86,10 @@
       <Button on:click={() => (showError = false)} variant="outlined" color="secondary">Ok</Button>
    </svelte:fragment>
 </Dialog>
+
+{#if showApp}
+   <svelte:component this={comp} />
+{/if}
 
 <style>
 </style>
