f02cda977b887751ed3eafd2af3fb50c7db0803e
Gregory Harder <gharder@hwec.com>
2021-11-15 10:57:50 -0600

    Added icons

--- public/img/icons/index.js
+++ public/img/icons/index.js
@@ -1,12 +1,27 @@
 import account_circle_svg from './account_circle.svg';
+import add_svg from './add.svg';
 import email_svg from './email.svg';
 import check_svg from './check.svg';
 import link_svg from './link.svg';
 import login_svg from './login.svg';
 import logout_svg from './logout.svg';
 import person_svg from './person.svg';
+import search_svg from './search.svg';
 import supervisor_account_svg from './supervisor_account.svg';
 import visibility_off_svg from './visibility_off.svg';
 import visibility_svg from './visibility.svg';
 
-export { account_circle_svg, email_svg, check_svg, link_svg, login_svg, logout_svg, person_svg, supervisor_account_svg, visibility_off_svg, visibility_svg };
+export {
+   account_circle_svg,
+   add_svg,
+   email_svg,
+   check_svg,
+   link_svg,
+   login_svg,
+   logout_svg,
+   person_svg,
+   search_svg,
+   supervisor_account_svg,
+   visibility_off_svg,
+   visibility_svg,
+};
