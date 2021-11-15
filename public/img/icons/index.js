a0962031ff644751305d269dbbf5e9a1eb8918a5
Gregory Harder <gharder@hwec.com>
2021-11-15 11:38:31 -0600

    Added icons for data table

--- public/img/icons/index.js
+++ public/img/icons/index.js
@@ -1,7 +1,12 @@
 import account_circle_svg from './account_circle.svg';
 import add_svg from './add.svg';
-import email_svg from './email.svg';
+import arrow_drop_up_svg from './arrow_drop_up.svg';
 import check_svg from './check.svg';
+import chevron_left_svg from './chevron_left.svg';
+import chevron_right_svg from './chevron_right.svg';
+import email_svg from './email.svg';
+import first_page_svg from './first_page.svg';
+import last_page_svg from './last_page.svg';
 import link_svg from './link.svg';
 import login_svg from './login.svg';
 import logout_svg from './logout.svg';
@@ -14,8 +19,13 @@ import visibility_svg from './visibility.svg';
 export {
    account_circle_svg,
    add_svg,
-   email_svg,
+   arrow_drop_up_svg,
    check_svg,
+   chevron_left_svg,
+   chevron_right_svg,
+   email_svg,
+   first_page_svg,
+   last_page_svg,
    link_svg,
    login_svg,
    logout_svg,
