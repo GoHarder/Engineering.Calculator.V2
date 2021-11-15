733580cb1630c92955e221ab4b9667f4c0f1b701
Gregory Harder <gharder@hwec.com>
2021-11-15 11:54:55 -0600

    Added icons for workbook row

--- public/img/icons/index.js
+++ public/img/icons/index.js
@@ -4,14 +4,18 @@ import arrow_drop_up_svg from './arrow_drop_up.svg';
 import check_svg from './check.svg';
 import chevron_left_svg from './chevron_left.svg';
 import chevron_right_svg from './chevron_right.svg';
+import delete_svg from './delete.svg';
 import email_svg from './email.svg';
+import file_copy_svg from './file_copy.svg';
 import first_page_svg from './first_page.svg';
 import last_page_svg from './last_page.svg';
 import link_svg from './link.svg';
 import login_svg from './login.svg';
 import logout_svg from './logout.svg';
+import more_vert_svg from './more_vert.svg';
 import person_svg from './person.svg';
 import search_svg from './search.svg';
+import share_svg from './share.svg';
 import supervisor_account_svg from './supervisor_account.svg';
 import visibility_off_svg from './visibility_off.svg';
 import visibility_svg from './visibility.svg';
@@ -23,14 +27,18 @@ export {
    check_svg,
    chevron_left_svg,
    chevron_right_svg,
+   delete_svg,
    email_svg,
+   file_copy_svg,
    first_page_svg,
    last_page_svg,
    link_svg,
    login_svg,
    logout_svg,
+   more_vert_svg,
    person_svg,
    search_svg,
+   share_svg,
    supervisor_account_svg,
    visibility_off_svg,
    visibility_svg,
