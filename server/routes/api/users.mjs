5047a679a9ba482cd6407e1c5250530da87b5f0e
Gregory Harder <gharder@hwec.com>
2021-11-15 10:12:49 -0600

    Added update password put route

--- server/routes/api/users.mjs
+++ server/routes/api/users.mjs
@@ -10,16 +10,19 @@ import { ObjectId } from 'mongodb';
 import { signToken, verifyToken } from '../../lib/crypto.mjs';
 import { app as appDB } from '../../data/mongodb/mongodb.mjs';
 import * as validate from '../../lib/validate.mjs';
-import { randomStr } from '../../lib/crypto.mjs';
+import { hash, randomStr } from '../../lib/crypto.mjs';
 import { sendResetPassword } from '../../lib/mailgun.mjs';
 
 /** The router for the module */
 export const router = express.Router();
 
-// Routes
-
-// - Get
-router.get('/', async (req, res) => {
+/**
+ * Checks the authorization header for a valid token
+ * @param req The request object
+ * @param res The response object
+ * @param next Forwards to next part of handler
+ */
+const checkAuth = (req, res, next) => {
    // Verify the token
    let token = req.headers.authorization?.split(' ')[1];
 
@@ -27,7 +30,16 @@ router.get('/', async (req, res) => {
 
    if (!token) return res.status(401).json({ message: 'Authorization is invalid' });
 
-   let { _id } = token;
+   req.token = token;
+
+   next();
+};
+
+// Routes
+
+// - Get
+router.get('/', checkAuth, async (req, res) => {
+   let { _id } = req.token;
 
    // Sanitize _ids
    _id = ObjectId(_id);
@@ -77,5 +89,34 @@ router.get('/:email', async (req, res) => {
    // Send user the reset email
    const sentEmail = await sendResetPassword(userDoc.email, reset);
 
+   if (!sentEmail) return res.status(500).json({ message: `Could not send email` });
+
    res.status(200).json({ token });
+});
+
+// - Put
+router.put('/', checkAuth, async (req, res) => {
+   const { body, token } = req;
+
+   // Check if reset strings are valid
+   if (body.reset !== token.reset) return res.status(401).json({ message: 'Authorization is invalid' });
+
+   // Validate the payload
+   if (!validate.password(body.password)) return res.status(400).json({ message: 'Password is invalid' });
+
+   // Update the user document
+   let updateInfo = undefined;
+
+   try {
+      const _id = ObjectId(token._id);
+      const hashedPassword = hash(body.password);
+
+      updateInfo = await appDB.collection('users').updateOne({ _id }, { $set: { hashedPassword } });
+   } catch (error) {
+      return res.status(500).json({ message: error.message });
+   }
+
+   if (updateInfo.modifiedCount === 0) return res.status(400).json({ message: 'Nothing was changed' });
+
+   res.status(204).send();
 });
