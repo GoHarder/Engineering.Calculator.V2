ef801db8063a4fdd34b6f464263ac5c19cf28523
Gregory Harder <gharder@hwec.com>
2021-11-18 13:33:31 -0600

    Added set token and set user

--- src/svelte/stores/user.js
+++ src/svelte/stores/user.js
@@ -28,8 +28,54 @@ const getUser = async (token) => {
    }
 };
 
+/**
+ * Saves the updated user information
+ * @param {object} payload The updated user information
+ */
+const saveUser = async (payload) => {
+   const token = localStorage.getItem('token');
+
+   fetchStore.loading(true);
+   let res, body;
+
+   try {
+      if (payload.password1 !== payload.password2) throw new Error('Passwords do not match');
+      if (payload.password1) {
+         payload.password = payload.password1;
+         delete payload.password1;
+         delete payload.password2;
+      }
+
+      const reqBody = JSON.stringify(payload);
+
+      res = await fetch(`/api/users`, {
+         method: 'PUT',
+         headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+         },
+         body: reqBody,
+      });
+
+      if (res.body && res.status !== 204) body = await res.json();
+
+      if (!res.ok) throw new Error(body.message);
+
+      fetchStore.loading(false);
+
+      return true;
+   } catch (error) {
+      fetchStore.setError({ res, error });
+      return false;
+   }
+};
+
 // Creates the custom store and sets up renewal loop
-const { subscribe, set: _set } = writable(localStorage.getItem('token'), async (set) => {
+const {
+   subscribe,
+   set: _set,
+   update: _update,
+} = writable(localStorage.getItem('token'), async (set) => {
    // Set the default state
    set(undefined);
 
@@ -83,13 +129,31 @@ const { subscribe, set: _set } = writable(localStorage.getItem('token'), async (
  * Set the security token
  * @param {string} token The security token to store
  */
-const set = async (token) => {
+const setToken = async (token) => {
    // Set the token
    localStorage.setItem('token', token);
 
    // Get the user and set store
    const user = await getUser(token);
    _set(user);
+};
+
+/**
+ * Sets new user data in the store
+ * @param {object} update The new user data
+ */
+const setUser = async (update) => {
+   const userSaved = await saveUser(update);
+
+   if (userSaved) {
+      _update((store) => {
+         update = { ...store, ...update };
+
+         return update;
+      });
+   }
+
+   return userSaved;
 };
 
 /** Deletes the user data */
@@ -102,6 +166,7 @@ const destroy = () => {
 // export the store object
 export default {
    destroy,
-   set,
+   setToken,
+   setUser,
    subscribe,
 };
