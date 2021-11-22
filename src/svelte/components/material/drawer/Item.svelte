895ebd7909d82359ae61337589aa3142a31021ec
Gregory Harder <gharder@hwec.com>
2021-11-22 07:27:13 -0600

    Added item to drawer

---
+++ src/svelte/components/material/drawer/Item.svelte
@@ -0,0 +1,30 @@
+<script>
+   // Components
+   // Stores
+   // Properties
+   export let href = undefined;
+
+   // Methods
+   // Constants
+   // Variables
+   // Subscriptions
+   // Contexts
+   // Reactive Rules
+   // Events
+   const onClick = (event) => {
+      event.preventDefault();
+      history.pushState({ path: href }, '');
+   };
+
+   // Lifecycle
+</script>
+
+<a on:click={onClick} class="mdc-deprecated-list-item" {href}>
+   <span class="mdc-list-item__ripple" />
+   <slot name="icon" />
+   <!-- <i class="material-icons mdc-list-item__graphic" aria-hidden="true">inbox</i> -->
+   <span class="mdc-list-item__text"><slot /></span>
+</a>
+
+<style>
+</style>
