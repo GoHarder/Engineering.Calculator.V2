a5a9d050aed1ead680a426ca3d714765427b93dc
Gregory Harder <gharder@hwec.com>
2021-11-23 16:29:58 -0600

    Added material icons font

---
+++ src/svelte/components/material/common/Icon.svelte
@@ -0,0 +1,18 @@
+<script>
+   // Components
+   // Stores
+   // Properties
+   // Methods
+   // Constants
+   // Variables
+   // Subscriptions
+   // Contexts
+   // Reactive Rules
+   // Events
+   // Lifecycle
+</script>
+
+<i class="material-icons"><slot /></i>
+
+<style>
+</style>
