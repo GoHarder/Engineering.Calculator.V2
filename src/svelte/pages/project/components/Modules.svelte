0a581d80245739c27a10a578ce5070753f5e5564
Gregory Harder <gharder@hwec.com>
2021-11-23 12:06:36 -0600

    Added basic project form class

---
+++ src/svelte/pages/project/components/Modules.svelte
@@ -0,0 +1,18 @@
+<script>
+   // Components
+   // Stores
+   // Properties
+   // Methods
+   // Constants
+   // Variables
+   // Subscriptions
+   // Contexts
+   // Reactive Rules
+   // Events
+   // Lifecycle
+</script>
+
+<section class="project-form">Modules</section>
+
+<style>
+</style>
---
+++ src/svelte/pages/project/components/Requirements.svelte
@@ -0,0 +1,18 @@
+<script>
+   // Components
+   // Stores
+   // Properties
+   // Methods
+   // Constants
+   // Variables
+   // Subscriptions
+   // Contexts
+   // Reactive Rules
+   // Events
+   // Lifecycle
+</script>
+
+<section class="project-form">Requirements</section>
+
+<style>
+</style>
