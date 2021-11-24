13d931bca885ad7f7fee4b012ccd2296579d5ca8
Gregory Harder <gharder@hwec.com>
2021-11-24 08:48:44 -0600

    Added tooltips to icons

--- src/svelte/components/material/common/Icon.svelte
+++ src/svelte/components/material/common/Icon.svelte
@@ -1,18 +1,32 @@
 <script>
+   import { filterProps, randomId } from '../../lib';
+
    // Components
+   import ToolTip from '../tool-tip/ToolTip.svelte';
+
    // Stores
    // Properties
+   export let toolTip = undefined;
+
    // Methods
    // Constants
+   let id = `icon-${randomId()}`;
+
    // Variables
    // Subscriptions
    // Contexts
    // Reactive Rules
+   $: props = filterProps($$props, ['toolTip']);
+
    // Events
    // Lifecycle
 </script>
 
-<i class="material-icons"><slot /></i>
+<i on:click {...props} data-tooltip-id={id}><slot /></i>
+
+{#if toolTip}
+   <ToolTip {id}>{toolTip}</ToolTip>
+{/if}
 
 <style>
 </style>
