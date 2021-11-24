d9734d48bf7606bfbd2aa8a295daa2d64dafe2a2
Gregory Harder <gharder@hwec.com>
2021-11-24 09:13:48 -0600

    Added icon to library

--- src/svelte/components/material/list/index.js
+++ src/svelte/components/material/list/index.js
@@ -1,3 +1,4 @@
 export { default as Divider } from '../common/Divider.svelte';
+export { default as Icon } from '../common/Icon.svelte';
 export { default as Item } from '../common/Item.svelte';
 export { default as List } from './List.svelte';
