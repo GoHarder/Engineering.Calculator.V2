8ce1a183d7b0e11b81ee5c7a457a6ad60dc59ec1
Gregory Harder <gharder@hwec.com>
2021-11-29 15:29:00 -0600

    Added requirements form

--- src/svelte/pages/project/components/Requirements.svelte
+++ src/svelte/pages/project/components/Requirements.svelte
@@ -1,18 +1,367 @@
 <script>
+   import { onDestroy } from 'svelte';
+
+   import { round } from 'lib/math.mjs';
+
    // Components
+   import { Checkbox } from 'components/material/checkbox';
+   import { HelperText, Input, InputLength, InputNumber } from 'components/material/input';
+   import { Option, Select } from 'components/material/select';
+
    // Stores
+   import projectStore from 'stores/project';
+
    // Properties
    // Methods
+   const filterIbcTable = (category, ip, sds) => {
+      let result = ibcTable.filter((row) => row.name === category);
+
+      if (result.length === 1) return result[0];
+
+      result = result.filter((row) => row.ip === ip);
+
+      if (result.length === 1) return result[0];
+
+      return result.find((row) => row.minSDS <= sds && sds < row.maxSDS);
+   };
+
    // Constants
+   const codeOpts = [
+      { name: 'ASME A17-1 2019', ibc: true },
+      { name: 'ASME A17-1 2016', ibc: true },
+      { name: 'ASME A17-1 2013', ibc: true },
+      { name: 'ASME A17-1 2012', ibc: false },
+      { name: 'ASME A17-1 2010', ibc: false },
+      { name: 'ASME A17-1 2005S', ibc: false },
+      { name: 'ASME A17-1 2000', ibc: false },
+   ];
+
+   const freightClassOpts = [
+      { name: 'None', types: ['Passenger'] },
+      { name: 'A', types: ['Passenger', 'Freight'] },
+      { name: 'B-Auto', types: ['Freight'] },
+      { name: 'B-Truck', types: ['Freight'] },
+      { name: 'C1', types: ['Passenger', 'Freight'] },
+      { name: 'C2', types: ['Passenger', 'Freight'] },
+      { name: 'C3', types: ['Passenger', 'Freight'] },
+   ];
+
+   const ibcTable = [
+      { name: 'A', ip: 0, minSDS: 0, maxSDS: 0, zone: 0 },
+      { name: 'B', ip: 0, minSDS: 0, maxSDS: 0, zone: 0 },
+      { name: 'C', ip: 1, minSDS: 0, maxSDS: 0, zone: 0 },
+      { name: 'C', ip: 1.5, minSDS: 0, maxSDS: 0.496, zone: 2 },
+      { name: 'C', ip: 1.5, minSDS: 0.496, maxSDS: 0.993, zone: 3 },
+      { name: 'C', ip: 1.5, minSDS: 0.993, maxSDS: 5, zone: 4 },
+      { name: 'D', ip: 1, minSDS: 0, maxSDS: 0.745, zone: 2 },
+      { name: 'D', ip: 1, minSDS: 0.745, maxSDS: 1.487, zone: 3 },
+      { name: 'D', ip: 1, minSDS: 1.487, maxSDS: 5, zone: 4 },
+      { name: 'D', ip: 1.5, minSDS: 0, maxSDS: 0.496, zone: 0 },
+      { name: 'D', ip: 1.5, minSDS: 0.496, maxSDS: 0.993, zone: 0 },
+      { name: 'D', ip: 1.5, minSDS: 0.993, maxSDS: 5, zone: 0 },
+      { name: 'E', ip: 1, minSDS: 0, maxSDS: 0.745, zone: 2 },
+      { name: 'E', ip: 1, minSDS: 0.745, maxSDS: 1.487, zone: 3 },
+      { name: 'E', ip: 1, minSDS: 1.487, maxSDS: 5, zone: 4 },
+      { name: 'E', ip: 1.5, minSDS: 0, maxSDS: 0.496, zone: 0 },
+      { name: 'E', ip: 1.5, minSDS: 0.496, maxSDS: 0.993, zone: 0 },
+      { name: 'E', ip: 1.5, minSDS: 0.993, maxSDS: 5, zone: 0 },
+      { name: 'F', ip: 1, minSDS: 0, maxSDS: 0.745, zone: 2 },
+      { name: 'F', ip: 1, minSDS: 0.745, maxSDS: 1.487, zone: 3 },
+      { name: 'F', ip: 1, minSDS: 1.487, maxSDS: 5, zone: 4 },
+      { name: 'F', ip: 1.5, minSDS: 0, maxSDS: 0.496, zone: 0 },
+      { name: 'F', ip: 1.5, minSDS: 0.496, maxSDS: 0.993, zone: 0 },
+      { name: 'F', ip: 1.5, minSDS: 0.993, maxSDS: 5, zone: 0 },
+   ];
+
    // Variables
+
+   // - Globals
+   let capacity;
+   let code;
+   let counterbalance;
+   let freightClass;
+   let ibcCategory;
+   let ip;
+   let loadingType;
+   let overallTravel;
+   let roping;
+   let sds;
+   let seismicZone;
+   let speed;
+   let useIbc;
+
+   // - Project
+   let metric;
+
+   // - Objects
+   let codeObj;
+
    // Subscriptions
+   const clearProject = projectStore.subscribe((store) => {
+      const { globals } = store;
+
+      capacity = globals?.capacity ?? 0;
+      code = globals?.code ?? 'ASME A17-1 2010';
+      counterbalance = globals.counterbalance ? globals.counterbalance * 100 : 40;
+      freightClass = globals?.loading?.freight ?? 'None';
+      ibcCategory = globals?.seismic?.category ?? 'A';
+      ip = globals?.seismic?.ip ?? 1;
+      loadingType = globals?.loading?.type ?? 'Passenger';
+      overallTravel = globals?.overallTravel ?? 0;
+      roping = globals?.roping ?? 1;
+      sds = globals?.seismic?.sds ?? 0;
+      seismicZone = globals?.seismic?.zone ?? 0;
+      speed = globals?.speed ?? 0;
+      useIbc = globals?.seismic?.useIbc ?? false;
+
+      metric = store?.metric ?? false;
+
+      console.log(store);
+   });
+
    // Contexts
    // Reactive Rules
+   $: ibcObj = filterIbcTable(ibcCategory, ip, sds);
+
+   $: if (useIbc && ibcObj) {
+      seismicZone = ibcObj?.zone ?? 0;
+   }
+
+   $: machineSpeed = speed * roping;
+   $: machineCapacity = capacity / roping;
+
    // Events
    // Lifecycle
+   onDestroy(() => {
+      const globals = {
+         capacity,
+         code,
+         counterbalance: round(counterbalance / 100, 3),
+         loading: {
+            type: loadingType,
+            freight: freightClass,
+         },
+         machine: {
+            capacity: machineCapacity,
+            speed: machineSpeed,
+         },
+         overallTravel,
+         roping,
+         seismic: {
+            category: ibcCategory,
+            ip,
+            sds,
+            useIbc,
+            zone: seismicZone,
+         },
+         speed,
+      };
+
+      if (!useIbc) {
+         delete globals.seismic.category;
+         delete globals.seismic.ip;
+         delete globals.seismic.sds;
+      }
+
+      projectStore.update({ globals });
+      clearProject();
+   });
 </script>
 
-<section class="project-form">Requirements</section>
+<section class="project-form">
+   <div class="main-grid">
+      <p>Enter the project requirements and proceed to the next step</p>
 
-<style>
+      <InputNumber bind:value={capacity} fullWidth gridArea="capacity" label="Capacity" max="30000" min="1" {metric} type="weight">
+         <svelte:fragment slot="helperText">
+            <HelperText validation>Invalid Capacity</HelperText>
+         </svelte:fragment>
+      </InputNumber>
+
+      <div class="counterbalance">
+         <Input bind:value={counterbalance} fullWidth label="Counterbalance" list="counterbalance" max="50" min="40" {metric} step="2.5" suffix="%" type="number">
+            <svelte:fragment slot="helperText">
+               <HelperText validation>Invalid Counterbalance %</HelperText>
+               <datalist id="counterbalance">
+                  <option value="40" />
+                  <option value="45" />
+                  <option value="42.5" />
+                  <option value="50" />
+               </datalist>
+            </svelte:fragment>
+         </Input>
+      </div>
+
+      <InputNumber bind:value={speed} fullWidth gridArea="speed" label="Car Speed" max="1500" min="1" {metric} type="speed">
+         <svelte:fragment slot="helperText">
+            <HelperText validation>Invalid Car Speed</HelperText>
+         </svelte:fragment>
+      </InputNumber>
+
+      <InputLength bind:value={overallTravel} fullWidth gridArea="travel" label="Overall Travel" {metric}>
+         <svelte:fragment slot="helperText">
+            <HelperText validation>Invalid Overall Travel</HelperText>
+         </svelte:fragment>
+      </InputLength>
+
+      <Select bind:value={code} bind:selected={codeObj} gridArea="code" label="Governing Code" options={codeOpts}>
+         {#each codeOpts as { name } (name)}
+            <Option value={name}>{name}</Option>
+         {/each}
+      </Select>
+
+      <Select bind:value={loadingType} gridArea="type" label="Loading Type">
+         <Option value="Passenger">Passenger</Option>
+         <Option value="Freight">Freight</Option>
+      </Select>
+
+      <Select bind:value={freightClass} gridArea="class" label="Freight Class">
+         {#each freightClassOpts as { name, types } (name)}
+            {#if types.includes(loadingType)}
+               <Option value={name}>{name}</Option>
+            {/if}
+         {/each}
+      </Select>
+
+      <Select bind:value={roping} gridArea="roping" label="Roping" type="number">
+         <Option value="1">1:1</Option>
+         <Option value="2">2:1</Option>
+      </Select>
+
+      <Select bind:value={seismicZone} disabled={useIbc} gridArea="zone" label="Seismic Zone" type="number">
+         <Option value="0">0</Option>
+         <Option value="1">1</Option>
+         <Option value="2">2</Option>
+         <Option value="3">3</Option>
+         <Option value="4">4</Option>
+      </Select>
+
+      {#if codeObj?.ibc}
+         <div class="ibc checkbox">
+            <Checkbox bind:checked={useIbc} label="IBC Seismic Zone" />
+         </div>
+      {/if}
+   </div>
+
+   {#if useIbc}
+      <hr />
+
+      <div class="ibc-grid">
+         <p>IBC/ASCE 7 Seismic Parameters</p>
+
+         <Select bind:value={ibcCategory} gridArea="category" label="Design Category">
+            <Option value="A">A</Option>
+            <Option value="B">B</Option>
+            <Option value="C">C</Option>
+            <Option value="D">D</Option>
+            <Option value="E">E</Option>
+            <Option value="F">F</Option>
+         </Select>
+
+         {#if ibcObj.ip}
+            <Select bind:value={ip} gridArea="ip" label="IP" type="number">
+               <Option value="1">1</Option>
+               <Option value="1.5">1.5</Option>
+            </Select>
+         {/if}
+
+         {#if ibcObj.maxSDS}
+            <div class="sds">
+               <Input bind:value={sds} fullWidth label="SDS" min={0} max={5} step={0.001} type="number">
+                  <span slot="helperText">
+                     <HelperText validation>Invalid SDS</HelperText>
+                  </span>
+               </Input>
+            </div>
+         {/if}
+      </div>
+   {/if}
+</section>
+
+<style lang="scss">
+   .main-grid {
+      display: grid;
+      gap: 0.5em;
+      grid-template: {
+         columns: 1fr 1fr;
+         rows: 30px 70px 70px 70px 70px 70px;
+         areas: 'p p' 'capacity counter' 'speed travel' 'code type' 'class roping' 'zone ibc';
+      }
+   }
+
+   .ibc-grid {
+      margin: 1em 0 0;
+      display: grid;
+      gap: 0.5em;
+      grid-template: {
+         columns: 1fr 1fr;
+         rows: 30px 70px 70px;
+         areas: 'p p' 'category ip' 'sds .';
+      }
+   }
+
+   p {
+      grid-area: p;
+      margin: 0;
+   }
+
+   .counterbalance {
+      grid-area: counter;
+   }
+
+   .ibc {
+      grid-area: ibc;
+   }
+
+   .checkbox {
+      margin: 0.5em 0 0;
+   }
+
+   .sds {
+      grid-area: sds;
+   }
+
+   @media (min-width: 1100px) {
+      .main-grid {
+         display: grid;
+         gap: 0.5em;
+         grid-template: {
+            columns: 1fr 1fr 1fr;
+            rows: 30px 70px 70px 70px 70px;
+            areas: 'p p p' 'capacity counter speed' 'travel code type' 'class roping zone' 'ibc . .';
+         }
+      }
+
+      .ibc-grid {
+         display: grid;
+         gap: 0.5em;
+         grid-template: {
+            columns: 1fr 1fr 1fr;
+            rows: 30px 70px;
+            areas: 'p p p' 'category ip sds';
+         }
+      }
+   }
+
+   @media (min-width: 1400px) {
+      .main-grid {
+         display: grid;
+         gap: 0.5em;
+         grid-template: {
+            columns: 0.25fr 1fr 1fr 1fr 0.25fr;
+            rows: 30px 70px 70px 70px 70px;
+            areas: '. p p p .' '. capacity counter speed .' '. travel code type .' '. class roping zone .' '. ibc . . .';
+         }
+      }
+
+      .ibc-grid {
+         display: grid;
+         gap: 0.5em;
+         grid-template: {
+            columns: 0.25fr 1fr 1fr 1fr 0.25fr;
+            rows: 30px 70px;
+            areas: '. p p p .' '. category ip sds .';
+         }
+      }
+   }
 </style>
