6d59d294eb23478cdf94e6ca715004a9f20f8100
Gregory Harder <gharder@hwec.com>
2021-11-30 15:45:45 -0600

    Added blank calculator modules

---
+++ src/svelte/pages/calculator/modules/buffers/Buffers.svelte
@@ -0,0 +1,18 @@
+<script>
+   // Components
+   // Stores
+   // Properties
+   // Methods
+   // Constants
+   // Variables
+   // Subscriptions
+   // Contexts
+   // Reactive Rules
+   // Events
+   // Lifecycle
+</script>
+
+<template>Buffers</template>
+
+<style>
+</style>
