7d43448fc301f420764b02cc2f3dc8a5c9de85ca
Gregory Harder <gharder@hwec.com>
2021-11-30 12:54:19 -0600

    Added card to component library

---
+++ src/svelte/components/material/card/Card.svelte
@@ -0,0 +1,30 @@
+<script>
+   import { classList } from '../../lib';
+
+   // Components
+   // Stores
+   // Properties
+   // Methods
+   // Constants
+   // Variables
+   // Subscriptions
+   // Contexts
+   // Reactive Rules
+   $: divClass = classList(['mdc-card', $$props.class]);
+
+   // Events
+   // Lifecycle
+</script>
+
+<div class={divClass}>
+   <slot />
+</div>
+
+<style lang="scss" global>
+   @use '@material/card';
+   @include card.core-styles;
+
+   .mdc-card {
+      @include card.shape-radius(0px);
+   }
+</style>
