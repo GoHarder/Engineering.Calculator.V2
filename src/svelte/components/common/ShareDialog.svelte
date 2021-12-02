b6edd4f9fbc1e114efa6ea0a224dbe05f5716e60
Gregory Harder <gharder@hwec.com>
2021-12-02 14:25:17 -0600

    Added share dialog component

---
+++ src/svelte/components/common/ShareDialog.svelte
@@ -0,0 +1,52 @@
+<script>
+   // Components
+   import { Button } from 'components/material/button';
+   import { Dialog, Title } from 'components/material/dialog';
+   import { HelperText, Input } from 'components/material/input';
+
+   // Stores
+   // Properties
+   // Methods
+   // Constants
+   // Variables
+   let shareDialog;
+   let email;
+   let users;
+   let onShare2;
+
+   // Subscriptions
+   // Contexts
+   // Reactive Rules
+   // Events
+   // Lifecycle
+</script>
+
+<Dialog bind:show={shareDialog}>
+   <svelte:fragment slot="title">
+      <Title>Share Workbook</Title>
+   </svelte:fragment>
+
+   <span style="display:inline-block; margin-bottom: 0.5em;"> Enter the email of the user you want to share the workbook with </span>
+
+   <form id="share-form" on:submit={(event) => event.preventDefault()}>
+      <Input bind:value={email} label="Email" required type="email" list="user-list" fullWidth>
+         <svelte:fragment slot="helperText">
+            <HelperText validation>Invalid Email</HelperText>
+         </svelte:fragment>
+      </Input>
+
+      <datalist id="user-list">
+         {#each users as { _id, email, firstName, lastName } (_id)}
+            <option value={email}>{firstName} {lastName}</option>
+         {/each}
+      </datalist>
+   </form>
+
+   <svelte:fragment slot="actions">
+      <Button on:click={() => (shareDialog = false)} variant="outlined" color="secondary">Cancel</Button>
+      <Button on:click={onShare2} variant="outlined" color="secondary" type="submit" form="share-form">Ok</Button>
+   </svelte:fragment>
+</Dialog>
+
+<style>
+</style>
--- src/svelte/components/common/index.js
+++ src/svelte/components/common/index.js
@@ -1,3 +1,4 @@
 export { default as A } from './A.svelte';
 export { default as PasswordRequire } from './PasswordRequire.svelte';
+export { default as ShareDialog } from './ShareDialog.svelte';
 export { default as Svg } from './Svg.svelte';
