e8afbb3abf6da094f98bb4714cdb9002140bc600
Gregory Harder <gharder@hwec.com>
2021-12-06 14:22:26 -0600

    Added button note

---
+++ src/svelte/components/common/ButtonNote.svelte
@@ -0,0 +1,48 @@
+<script>
+   import { scale } from 'svelte/transition';
+
+   // Components
+   // Stores
+   // Properties
+   export let text = undefined;
+
+   // Methods
+   // Constants
+   // Variables
+   // Subscriptions
+   // Contexts
+   // Reactive Rules
+   // Events
+   // Lifecycle
+</script>
+
+<div class="wrapper">
+   <slot />
+   {#if text}
+      <p transition:scale>{text}</p>
+   {/if}
+</div>
+
+<style lang="scss">
+   @use './src/scss/theme' as vantage;
+
+   .wrapper {
+      display: inline-block;
+      position: relative;
+   }
+
+   p {
+      background-color: vantage.$primary;
+      color: vantage.$secondary;
+      position: absolute;
+      border-radius: 50px;
+      right: 0;
+      top: 0;
+      margin: 0 auto;
+      pointer-events: none;
+      width: 20px;
+      height: 20px;
+      font-size: 0.8rem;
+      text-align: center;
+   }
+</style>
--- src/svelte/components/common/index.js
+++ src/svelte/components/common/index.js
@@ -1,4 +1,5 @@
 export { default as A } from './A.svelte';
+export { default as ButtonNote } from './ButtonNote.svelte';
 export { default as PasswordRequire } from './PasswordRequire.svelte';
 export { default as ShareDialog } from './ShareDialog.svelte';
 export { default as Svg } from './Svg.svelte';
