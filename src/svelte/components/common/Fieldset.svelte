69a22653f22971e946327974fd88e0c7c08ad7d6
Gregory Harder <gharder@hwec.com>
2021-12-06 16:06:29 -0600

    Added fieldset

---
+++ src/svelte/components/common/Fieldset.svelte
@@ -0,0 +1,31 @@
+<script>
+   import { fade } from 'svelte/transition';
+
+   // Components
+   // Stores
+   // Properties
+   export let title = '';
+
+   // Methods
+   // Constants
+   // Variables
+   // Subscriptions
+   // Contexts
+   // Reactive Rules
+   // Events
+   // Lifecycle
+</script>
+
+<fieldset transition:fade>
+   <legend>{title}</legend>
+   <hr />
+   <slot />
+</fieldset>
+
+<style lang="scss">
+   @use 'src/scss/theme' as vantage;
+
+   fieldset {
+      @include vantage.fieldset;
+   }
+</style>
--- src/svelte/components/common/index.js
+++ src/svelte/components/common/index.js
@@ -1,5 +1,6 @@
 export { default as A } from './A.svelte';
 export { default as ButtonNote } from './ButtonNote.svelte';
+export { default as Fieldset } from './Fieldset.svelte';
 export { default as PasswordRequire } from './PasswordRequire.svelte';
 export { default as ShareDialog } from './ShareDialog.svelte';
 export { default as Svg } from './Svg.svelte';
