752b520078943b2d932bfb78e5b4e0290a539495
Gregory Harder <gharder@hwec.com>
2021-12-10 08:33:26 -0600

    Added get steel channel route

--- server/routes/api/engineering/platform.mjs
+++ server/routes/api/engineering/platform.mjs
@@ -7,7 +7,7 @@ import express from 'express';
 
 // Project Imports
 import { checkAuth, checkCache } from '../../../middleware/lib.mjs';
-import { _angle } from '../../../data/mongodb/pipelines/platform.mjs';
+import { _angle, getChannel } from '../../../data/mongodb/pipelines/platform.mjs';
 import { eng as engDB } from '../../../data/mongodb/mongodb.mjs';
 import { setEx } from '../../../data/redis/redis.mjs';
 
@@ -20,6 +20,22 @@ router.use([checkAuth, checkCache]);
 // Routes
 
 // - Get
+router.get('/steel/:steelType', async (req, res) => {
+   let docs = { message: 'ding' };
+
+   const _channel = getChannel(req.params.steelType);
+
+   try {
+      docs = await engDB.collection('steel').aggregate(_channel).toArray();
+   } catch (error) {
+      return res.status(500).json({ message: error.message });
+   }
+
+   await setEx(req.originalUrl, JSON.stringify(docs));
+
+   res.status(200).json(docs);
+});
+
 router.get('/wood', async (req, res) => {
    let docs;
 
