d32a47fe3ed3c680821cb7d5022174c639a1413c
Gregory Harder <gharder@hwec.com>
2021-12-14 13:03:52 -0600

    Added stock status options

---
+++ src/svelte/components/common/StockStatusOptions.svelte
@@ -0,0 +1,48 @@
+<script>
+   // Components
+   import { OptGroup, Option } from 'components/material/select';
+
+   // Stores
+   // Properties
+   export let options = [];
+
+   // Methods
+   // Constants
+   // Variables
+   // Subscriptions
+   // Contexts
+   // Reactive Rules
+   $: stockedOptions = options.filter((option) => option.stockStatus === 'Stocked');
+   $: availableOptions = options.filter((option) => option.stockStatus === 'Available');
+   $: checkOptions = options.filter((option) => option.stockStatus === 'Check');
+
+   // Events
+   // Lifecycle
+</script>
+
+{#if stockedOptions.length > 0}
+   <OptGroup label="Stocked">
+      {#each stockedOptions as { name } (name)}
+         <Option value={name}>{name}</Option>
+      {/each}
+   </OptGroup>
+{/if}
+
+{#if availableOptions.length > 0}
+   <OptGroup label="Available">
+      {#each availableOptions as { name } (name)}
+         <Option value={name}>{name}</Option>
+      {/each}
+   </OptGroup>
+{/if}
+
+{#if checkOptions.length > 0}
+   <OptGroup label="Check">
+      {#each checkOptions as { name } (name)}
+         <Option value={name}>{name}</Option>
+      {/each}
+   </OptGroup>
+{/if}
+
+<style>
+</style>
--- src/svelte/components/common/index.js
+++ src/svelte/components/common/index.js
@@ -3,4 +3,5 @@ export { default as ButtonNote } from './ButtonNote.svelte';
 export { default as Fieldset } from './Fieldset.svelte';
 export { default as PasswordRequire } from './PasswordRequire.svelte';
 export { default as ShareDialog } from './ShareDialog.svelte';
+export { default as StockStatusOptions } from './StockStatusOptions.svelte';
 export { default as Svg } from './Svg.svelte';
