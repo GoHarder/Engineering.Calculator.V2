4731a45f42262ebe4013a64f7076da8e9b5091b8
Gregory Harder <gharder@hwec.com>
2021-12-17 13:40:31 -0600

    Added rail sizes

---
+++ src/svelte/pages/calculator/modules/tables.js
@@ -0,0 +1,7 @@
+/**
+ * @module tables
+ * Common tables that are shared between modules
+ */
+
+/** Standard rail sizes */
+export const railSizes = [{ name: '6.25#' }, { name: '8#' }, { name: '11#' }, { name: '12#' }, { name: '15#' }, { name: '18.5#' }, { name: '22.5#' }, { name: '30#' }];
