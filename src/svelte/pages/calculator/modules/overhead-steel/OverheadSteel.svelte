e909808ed6bbef923e4ec785a5abca68a18cef89
Gregory Harder <gharder@hwec.com>
2022-01-17 15:13:27 -0600

    Added steel sets

--- src/svelte/pages/calculator/modules/overhead-steel/OverheadSteel.svelte
+++ src/svelte/pages/calculator/modules/overhead-steel/OverheadSteel.svelte
@@ -2,6 +2,12 @@
    import { onDestroy } from 'svelte';
 
    // Components
+   import { Fieldset } from 'components/common';
+   import { Button, Icon } from 'components/material/button';
+   import { Checkbox } from 'components/material/checkbox';
+
+   import SteelSet from './components/SteelSet.svelte';
+
    // Stores
    // Properties
    export let project;
@@ -22,17 +28,84 @@
    console.log(project);
 
    // Variables
+   let existing = false;
+   let supplied = false;
+   let steelSets = [];
+
    // Subscriptions
    // Contexts
    // Reactive Rules
    // Events
+   const onAddSet = () => {
+      const id = `set-${Date.now()}`;
+
+      const newSet = {
+         id,
+         label: 'Unnamed Set',
+         shape: '',
+         name: '',
+         members: [
+            {
+               i: 1,
+               id: `member-${Date.now()}`,
+               label: 'Unnamed Member',
+               length: 0,
+               pointLoads: [],
+               reactLoads: [],
+               links: [],
+               reactions: { rA: 0, rB: 0 },
+            },
+         ],
+         reactions: { rA: 0, rB: 0 },
+      };
+
+      steelSets = [...steelSets, newSet];
+   };
+
    // Lifecycle
    onDestroy(() => {
       updateModule();
    });
 </script>
 
-<template>Overhead Steel</template>
+<div class="container">
+   <Fieldset title="Properties">
+      <div class="properties">
+         <Checkbox bind:checked={existing} label="Existing Steel" disabled={supplied} />
+
+         <Checkbox bind:checked={supplied} label="H-W Supplied Steel" disabled={existing} />
+      </div>
+   </Fieldset>
+</div>
+
+<div class="container">
+   {#each steelSets as { id, name, shape, members, reactions, label } (id)}
+      <SteelSet bind:name bind:shape bind:members bind:label bind:reactions {id} {supplied} {existing} />
+   {/each}
+</div>
+
+<div class="container">
+   <Button on:click={onAddSet} variant="contained">
+      Add Steel
+      <svelte:fragment slot="trailingIcon">
+         <Icon>add</Icon>
+      </svelte:fragment>
+   </Button>
+</div>
 
 <style>
+   .container {
+      display: flex;
+      flex-wrap: wrap;
+      align-items: flex-start;
+      margin: 0.25em;
+      gap: 0.25em;
+   }
+
+   .properties {
+      display: flex;
+      flex-wrap: wrap;
+      align-items: flex-start;
+      gap: 0.5em;
+   }
 </style>
---
+++ src/svelte/pages/calculator/modules/overhead-steel/components/SteelSet.svelte
@@ -0,0 +1,86 @@
+<script>
+   // Components
+   import { Icon, IconButton } from 'components/material/button';
+
+   // Stores
+   // Properties
+   export let steel = {};
+   export let supplied = false;
+   export let existing = false;
+
+   export let id = '';
+   export let members = [];
+   export let name = '';
+   export let shape = '';
+   export let label = '';
+   export let reactions = { rA: 0, rB: 0 };
+
+   // Methods
+   // Constants
+   // Variables
+   let test = false;
+
+   // Subscriptions
+   // Contexts
+   // Reactive Rules
+   // Events
+   // Lifecycle
+</script>
+
+<div class="fieldset">
+   <header>
+      <p>Some sort of title</p>
+
+      <IconButton on={test} toggle class="density-2">
+         <Icon>copy_all</Icon>
+      </IconButton>
+
+      <IconButton on={test} toggle class="density-2">
+         <Icon class="mdc-icon-button__icon--on">minimize</Icon>
+         <Icon>maximize</Icon>
+      </IconButton>
+
+      <IconButton class="density-2">
+         <Icon>close</Icon>
+      </IconButton>
+   </header>
+
+   <hr />
+
+   {steel}
+   {supplied}
+   {existing}
+   {id}
+   {members}
+   {name}
+   {shape}
+   {label}
+   {reactions}
+</div>
+
+<style lang="scss">
+   @use './src/scss/theme' as vantage;
+
+   .fieldset {
+      @include vantage.paper;
+      padding: 5.6px 12px 10px;
+   }
+
+   header {
+      display: flex;
+      align-items: center;
+   }
+
+   p {
+      font-size: 1.1rem;
+      margin: 0;
+      margin-top: 6px;
+      margin-right: auto;
+
+      // margin: 6px 0 10px;
+   }
+
+   hr {
+      border: 1px solid vantage.$primary;
+   }
+</style>
