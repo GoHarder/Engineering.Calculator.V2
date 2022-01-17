d9c6e29cbc6cd31936734ec79473f82332f7511d
Gregory Harder <gharder@hwec.com>
2022-01-17 09:34:08 -0600

    Rough setup

--- src/svelte/pages/calculator/modules/overhead-steel/OverheadSteel.svelte
+++ src/svelte/pages/calculator/modules/overhead-steel/OverheadSteel.svelte
@@ -1,15 +1,35 @@
 <script>
+   import { onDestroy } from 'svelte';
+
    // Components
    // Stores
    // Properties
+   export let project;
+   export const updateModule = () => {
+      const globalData = {};
+
+      const moduleData = {};
+
+      project.globals = { ...project.globals, ...globalData };
+      project.modules.overheadSteel = moduleData;
+   };
+
    // Methods
    // Constants
+   const { metric, modules } = project;
+   const { overheadSteel: module } = modules;
+
+   console.log(project);
+
    // Variables
    // Subscriptions
    // Contexts
    // Reactive Rules
    // Events
    // Lifecycle
+   onDestroy(() => {
+      updateModule();
+   });
 </script>
 
 <template>Overhead Steel</template>
