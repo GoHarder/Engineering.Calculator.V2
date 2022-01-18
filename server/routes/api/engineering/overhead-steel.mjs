ef1515daf633f3284cb1a9cc90f91a07841283c4
Gregory Harder <gharder@hwec.com>
2022-01-18 08:54:39 -0600

    Added overhead steel route

---
+++ server/routes/api/engineering/overhead-steel.mjs
@@ -0,0 +1,43 @@
+/**
+ * @module overhead-steel The overhead steel route handler
+ */
+
+// NPM Imports
+import express from 'express';
+
+// Project Imports
+import { checkAuth, checkCache } from '../../../middleware/lib.mjs';
+import { getMachines } from '../../../data/mongodb/pipelines/machine.mjs';
+import { eng as engDB } from '../../../data/mongodb/mongodb.mjs';
+import { setEx } from '../../../data/redis/redis.mjs';
+
+/** The router for the module */
+export const router = express.Router();
+
+// Middleware
+router.use([checkAuth, checkCache]);
+
+// Routes
+
+// - Get
+router.get('/', async (req, res) => {
+   // try {
+   //    const _machines = getMachines(counterbalance, speed, capacity, type, location, roping);
+
+   //    machines = await engDB.collection('machines').aggregate(_machines).toArray();
+
+   //    maxRimWidth = await engDB.collection('machines').distinct('sheaves.rimWidth');
+
+   //    maxRimWidth = maxRimWidth[maxRimWidth.length - 1];
+   // } catch (error) {
+   //    return res.status(500).json({ message: error.message });
+   // }
+
+   // const docs = { machines, maxRimWidth };
+
+   // await setEx(req.originalUrl, JSON.stringify(docs));
+
+   const docs = { message: 'Ding' };
+
+   res.status(200).json(docs);
+});
