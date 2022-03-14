acd273d7c4a707ff33cb4ab8678b629a4fd47711
Gregory Harder <gharder@hwec.com>
2022-03-14 13:17:32 -0500

    Adjusted border

--- src/svelte/pages/calculator/modules/overhead-steel/components/Load.svelte
+++ src/svelte/pages/calculator/modules/overhead-steel/components/Load.svelte
@@ -31,43 +31,52 @@
    // Lifecycle
 </script>
 
-<header>
-   <p class="label" bind:textContent={label} contenteditable="true" data-tooltip-id={id} />
-   <ToolTip {id}>Edit Label</ToolTip>
+<div>
+   <header>
+      <p class="label" bind:textContent={label} contenteditable="true" data-tooltip-id={id} />
+      <ToolTip {id}>Edit Label</ToolTip>
 
-   <IconButton bind:on={show} toggle class="density-3" toolTip={show ? 'Minimize' : 'Maximize'}>
-      <Icon class="mdc-icon-button__icon--on">minimize</Icon>
-      <Icon>maximize</Icon>
-   </IconButton>
+      <IconButton bind:on={show} toggle class="density-3" toolTip={show ? 'Minimize' : 'Maximize'}>
+         <Icon class="mdc-icon-button__icon--on">minimize</Icon>
+         <Icon>maximize</Icon>
+      </IconButton>
 
-   <IconButton on:click={onDelete} class="density-3" toolTip="Delete">
-      <Icon>close</Icon>
-   </IconButton>
-</header>
+      <IconButton on:click={onDelete} class="density-3" toolTip="Delete">
+         <Icon>close</Icon>
+      </IconButton>
+   </header>
 
-{#if show}
-   {#if !reaction}
-      <Select bind:value={type} label="Type">
-         <Option value="Dead">Dead</Option>
-         <Option value="Live">Live</Option>
-      </Select>
+   {#if show}
+      {#if !reaction}
+         <Select bind:value={type} label="Type">
+            <Option value="Dead">Dead</Option>
+            <Option value="Live">Live</Option>
+         </Select>
+      {/if}
+
+      <InputLength bind:value={length} label="Length From R<sub>a</sub>" />
+
+      {#if !reaction}
+         <InputNumber bind:value={weight} label="Weight" type="weight" />
+      {/if}
    {/if}
-
-   <InputLength bind:value={length} label="Length From R<sub>a</sub>" />
-
-   {#if !reaction}
-      <InputNumber bind:value={weight} label="Weight" type="weight" />
-   {/if}
-{/if}
+</div>
 
 <hr />
 
 <style lang="scss">
    @use './src/scss/theme' as vantage;
 
+   div {
+      margin-left: 8px;
+   }
    header {
       display: flex;
       align-items: center;
+   }
+
+   hr {
+      margin: 0;
    }
 
    .label {
