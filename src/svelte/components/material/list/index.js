cd003799c1ce7222ac8ccb143d310faa811ee447
Gregory Harder <gharder@hwec.com>
2022-03-15 11:50:49 -0500

    Moved list subheader to the common directory

--- src/svelte/components/material/list/index.js
+++ src/svelte/components/material/list/index.js
@@ -2,3 +2,4 @@ export { default as Divider } from '../common/Divider.svelte';
 export { default as Icon } from '../common/Icon.svelte';
 export { default as Item } from '../common/Item.svelte';
 export { default as List } from './List.svelte';
+export { default as SubHeader } from '../common/SubHeader.svelte';
