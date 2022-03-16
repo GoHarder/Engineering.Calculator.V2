bce517863b74ed54475685e84eb321bcef436333
Gregory Harder <gharder@hwec.com>
2022-03-16 08:27:07 -0500

    Added reaction selection

--- src/svelte/pages/calculator/modules/overhead-steel/OverheadSteel.svelte
+++ src/svelte/pages/calculator/modules/overhead-steel/OverheadSteel.svelte
@@ -2,13 +2,14 @@
    import { onDestroy, onMount } from 'svelte';
 
    import { clone, deepMerge } from 'lib/main.mjs';
+   import { floorInc } from 'lib/math.mjs';
 
    // Components
    import { Fieldset } from 'components/common';
    import { Button, Icon } from 'components/material/button';
    import { Checkbox } from 'components/material/checkbox';
    import { Dialog, Title } from 'components/material/dialog';
-   import { OptGroup, Option, Select } from 'components/material/select';
+   import { Item, List, SubHeader } from 'components/material/list';
 
    import SteelSet from './components/SteelSet.svelte';
 
@@ -22,7 +23,7 @@
          existing,
          supplied,
          steelSets,
-         // reactLinks,
+         reactLinks,
       };
 
       // project.globals = { ...project.globals, ...globalData };
@@ -36,22 +37,72 @@
    const { overheadSteel: module } = modules;
 
    // Variables
-
    let existing = module?.existing ?? false;
    let supplied = module?.existing ?? false;
    let steelSets = module?.steelSets ?? [];
-   // let reactLinks = module?.reactLinks ?? [];
+   let reactLinks = module?.reactLinks ?? [];
+
+   // - Reaction dialog
+   let reactionDialog = false;
+   let member1 = undefined;
+   let reaction = undefined;
 
    // Subscriptions
    // Contexts
    // Reactive Rules
-   // Events
+   $: reactOptions = clone(steelSets).reduce((output, set) => {
+      const label = set.members.length === 1 ? set.members[0].label : set.label;
+      const from = set.id;
+      const members = set.members.map((member) => member.id);
+      const option = { id: `link-${Date.now()}`, label, ...set.reactions, from, to: member1 };
 
+      if (!members.includes(member1?.member ?? '')) output.push(option);
+
+      return output;
+   }, []);
+
+   $: if (reactLinks.length > 0) {
+      const update = clone(steelSets);
+      const toDelete = [];
+
+      reactLinks.forEach((link) => {
+         const from = update.find((set) => set.id === link.from);
+         const toSet = update.find((set) => set.id === link.to.set);
+         const toMember = toSet?.members.find((member) => member.id === link.to.member);
+         const reactLoad = toMember?.reactLoads.find((load) => load.id === link.id);
+
+         if (!toMember) toDelete.push(link.id);
+
+         if (reactLoad) {
+            reactLoad.weight = from.reactions[link.use];
+            reactLoad.label = `${link.label} ${link.use === 'rA' ? 'Ra' : 'Rb'}`;
+         } else {
+            const newLoad = {
+               id: link.id,
+               label: `${link.label} ${link.use === 'rA' ? 'Ra' : 'Rb'}`,
+               weight: from.reactions[link.use],
+               length: 0,
+               show: true,
+               type: 'Dead',
+            };
+
+            toMember?.reactLoads.push(newLoad);
+         }
+      });
+
+      if (toDelete.length > 0) {
+         reactLinks = [...reactLinks].filter((link) => !toDelete.includes(link.id));
+      }
+
+      steelSets = update;
+   }
+
+   // Events
    const onAddSet = () => {
       const id = `set-${Date.now()}`;
 
       const newSet = {
-         show: false,
+         delay: 0,
          axis: 'x',
          id,
          label: 'Unnamed Set',
@@ -66,7 +117,6 @@
                lengthRb: 0,
                pointLoads: [],
                reactLoads: [],
-               links: [],
                reactions: { rA: 0, rB: 0 },
                o_lengthRb: false,
             },
@@ -77,7 +127,33 @@
       steelSets = [...steelSets, newSet];
    };
 
-   const onDeleteSet = (event) => (steelSets = [...steelSets].filter((set) => set.id !== event.detail));
+   const onAddReaction1 = (event) => {
+      member1 = event.detail;
+      reactionDialog = true;
+   };
+
+   const onAddReaction2 = () => {
+      reactLinks = [...reactLinks, reaction];
+      member1 = undefined;
+      reaction = undefined;
+      reactionDialog = false;
+   };
+
+   const onDeleteSet = (event) => {
+      reactLinks = [...reactLinks].filter((link) => link.to.set !== event.detail);
+      steelSets = [...steelSets].filter((set) => set.id !== event.detail);
+   };
+
+   const onDeleteReaction = (event) => {
+      reactLinks = [...reactLinks].filter((link) => link.id !== event.detail);
+   };
+
+   const onSelectReact = (event) => {
+      const { index } = event.detail;
+      const option = reactOptions[floorInc(index, 2)];
+      option.use = index % 2 === 0 ? 'rA' : 'rb';
+      reaction = option;
+   };
 
    // Lifecycle
    onMount(() => {
@@ -85,7 +161,7 @@
       const { overheadSteel: module } = modules;
 
       module.steelSets = module.steelSets.map((set, i) => {
-         set.delay = i + 1 * 2000;
+         set.delay = (i + 1) * 2000;
          return set;
       });
    });
@@ -94,6 +170,37 @@
       updateModule();
    });
 </script>
+
+<Dialog bind:show={reactionDialog}>
+   <svelte:fragment slot="title">
+      {#if reactOptions.length > 0}
+         <Title>Add Reaction</Title>
+      {:else}
+         <Title class="error">No Reactions</Title>
+      {/if}
+   </svelte:fragment>
+
+   {#if reactOptions.length === 0}
+      <p>No reactions are available</p>
+   {:else}
+      <List on:action={onSelectReact} dense singleSelection>
+         {#each reactOptions as { label, rA, rB }}
+            <SubHeader {label}>
+               <Item>Ra {rA}lb</Item>
+               <Item>Rb {rB}lb</Item>
+            </SubHeader>
+         {/each}
+      </List>
+   {/if}
+
+   <svelte:fragment slot="actions">
+      <Button on:click={() => (reactionDialog = false)} variant="outlined" color="secondary">Cancel</Button>
+
+      {#if reactOptions.length > 0 && reaction}
+         <Button on:click={onAddReaction2} variant="outlined" color="secondary">Ok</Button>
+      {/if}
+   </svelte:fragment>
+</Dialog>
 
 <div class="container">
    <Fieldset title="Properties">
@@ -107,7 +214,21 @@
 
 <div class="container">
    {#each steelSets as { axis, delay, id, name, shape, members, reactions, label } (id)}
-      <SteelSet on:delete={onDeleteSet} bind:axis bind:label bind:members bind:name bind:shape {existing} {delay} {id} {supplied} />
+      <SteelSet
+         on:addReaction={onAddReaction1}
+         on:deleteReaction={onDeleteReaction}
+         on:delete={onDeleteSet}
+         bind:axis
+         bind:label
+         bind:members
+         bind:name
+         bind:reactions
+         bind:shape
+         {existing}
+         {delay}
+         {id}
+         {supplied}
+      />
    {/each}
 </div>
 
