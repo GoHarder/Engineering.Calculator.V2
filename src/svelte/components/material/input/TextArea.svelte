be2479a1cb3902041e402b072a2547a73397919d
Gregory Harder <gharder@hwec.com>
2022-03-18 15:29:50 -0500

    Added height to dialog

--- src/svelte/components/material/input/TextArea.svelte
+++ src/svelte/components/material/input/TextArea.svelte
@@ -33,7 +33,7 @@
 <label bind:this={labelEle} for="test-id" class="mdc-text-field mdc-text-field--filled mdc-text-field--textarea mdc-text-field--no-label">
    <span class="mdc-text-field__ripple" />
    <span class="mdc-text-field__resizer">
-      <textarea bind:value class="mdc-text-field__input" rows="8" cols="40" aria-label="Label" id="test-id" {...props} />
+      <textarea bind:value class="mdc-text-field__input" rows="14" cols="40" aria-label="Label" id="test-id" {...props} />
    </span>
    <span class="mdc-line-ripple" />
 </label>
