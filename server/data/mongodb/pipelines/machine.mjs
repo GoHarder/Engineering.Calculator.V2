3aaa2aef99cb0ec4b02f044247c20a8653369d12
Gregory Harder <gharder@hwec.com>
2022-04-12 15:48:13 -0500

    Updated database call to include rope grippers

--- server/data/mongodb/pipelines/machine.mjs
+++ server/data/mongodb/pipelines/machine.mjs
@@ -17,6 +17,7 @@ export const getMachines = (counterbalance, speed, capacity, type, location, rop
    const validCB = ['capAt40_0', 'capAt42_5', 'capAt45_0', 'capAt50_0'].includes(cbKey);
 
    const match = { speed: { $gte: speed } };
+
    let cond = { $gte: ['$$limit.speed', speed] };
 
    if (validCB) {
@@ -27,11 +28,30 @@ export const getMachines = (counterbalance, speed, capacity, type, location, rop
    return [
       { $unwind: { path: '$sheaves' } },
       { $match: { type, location, roping, 'sheaves.limits': { $elemMatch: match } } },
+      { $unwind: { path: '$ropeGrippers' } },
       {
          $addFields: {
-            'sheaves.limits': {
-               $filter: { input: '$sheaves.limits', as: 'limit', cond },
+            'sheaves.limits': { $filter: { input: '$sheaves.limits', as: 'limit', cond } },
+            'ropeGrippers.ropings': { $filter: { input: '$ropeGrippers.ropings', as: 'nth', cond: { $eq: ['$$nth.roping', 1] } } },
+         },
+      },
+      {
+         $project: {
+            _id: 1,
+            name: 1,
+            bases: 1,
+            centerOfGravity: 1,
+            ropeGrippers: {
+               name: 1,
+               maxCapacity: { $first: '$ropeGrippers.ropings.maxCapacity' },
+               maxLoad: { $first: '$ropeGrippers.ropings.maxLoad' },
+               maxSpeed: { $first: '$ropeGrippers.ropings.maxSpeed' },
+               minCapacity: { $first: '$ropeGrippers.ropings.minCapacity' },
+               minLoad: { $first: '$ropeGrippers.ropings.minLoad' },
+               outToOut: 1,
+               weight: 1,
             },
+            sheaves: 1,
          },
       },
       {
@@ -40,13 +60,31 @@ export const getMachines = (counterbalance, speed, capacity, type, location, rop
             name: { $first: '$name' },
             bases: { $first: '$bases' },
             centerOfGravity: { $first: '$centerOfGravity' },
-            location: { $first: '$location' },
-            roping: { $first: '$roping' },
-            sheaves: { $push: '$sheaves' },
-            type: { $first: '$type' },
+            ropeGrippers: { $addToSet: '$ropeGrippers' },
+            sheaves: { $addToSet: '$sheaves' },
          },
       },
-      { $project: { location: 0, roping: 0, type: 0, 'sheaves.limits': 0 } },
+      {
+         $addFields: {
+            ropeGrippers: {
+               $filter: {
+                  input: '$ropeGrippers',
+                  as: 'nth',
+                  cond: { $and: [{ $gte: ['$$nth.maxSpeed', speed] }, { $gte: ['$$nth.maxCapacity', capacity] }, { $lte: ['$$nth.minCapacity', capacity] }] },
+               },
+            },
+         },
+      },
+      {
+         $project: {
+            _id: 1,
+            name: 1,
+            bases: 1,
+            centerOfGravity: 1,
+            ropeGrippers: { name: 1, maxLoad: 1, minLoad: 1, outToOut: 1, weight: 1 },
+            sheaves: { diameter: 1, maxGroovePressure: 1, name: 1, rimWidth: 1, maxSystemLoad: 1 },
+         },
+      },
       { $sort: { name: 1 } },
    ];
 };
