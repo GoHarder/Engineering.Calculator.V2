6ebbed5c1ef0cb6985d19f58f50aacba572bd55c
Gregory Harder <gharder@hwec.com>
2022-05-18 11:48:49 -0500

    Fixed bug when its being destoryed

--- src/svelte/components/material/progress/CircularProgress.svelte
+++ src/svelte/components/material/progress/CircularProgress.svelte
@@ -12,10 +12,44 @@
 
    // Methods
    // Constants
+   const sizes = {
+      small: {
+         style: 'width:24px;height:24px;',
+         viewBox: '0 0 24 24',
+         cx: '12',
+         cy: '12',
+         r: '8.75',
+         width1: '2.5',
+         width2: '2',
+         dash1: '54.978',
+         dash2: '27.489',
+      },
+      medium: {
+         style: 'width:36px;height:36px;',
+         viewBox: '0 0 32 32',
+         cx: '16',
+         cy: '16',
+         r: '12.5',
+         width1: '3',
+         width2: '2.4',
+         dash1: '78.54',
+         dash2: '39.27',
+      },
+      large: {
+         style: 'width:48px;height:48px;',
+         viewBox: '0 0 48 48',
+         cx: '24',
+         cy: '24',
+         r: '18',
+         width1: '4',
+         width2: '3.2',
+         dash1: '113.097',
+         dash2: '56.549',
+      },
+   };
+
    // Variables
-   let divEleLarge;
-   let divEleMedium;
-   let divEleSmall;
+   let divEle;
    let CircularProgress;
 
    // Subscriptions
@@ -29,17 +63,7 @@
    // Events
    // Lifecycle
    onMount(() => {
-      const sizes = {
-         small: divEleSmall,
-         medium: divEleMedium,
-         large: divEleLarge,
-      };
-
-      CircularProgress = new MDCCircularProgress(sizes[size]);
-
-      for (const key in sizes) {
-         if (key !== size) sizes[key].remove();
-      }
+      CircularProgress = new MDCCircularProgress(divEle);
    });
 
    onDestroy(() => {
@@ -47,108 +71,57 @@
    });
 </script>
 
-<div
-   bind:this={divEleLarge}
-   class="mdc-circular-progress"
-   style="width:48px;height:48px;"
-   role="progressbar"
-   aria-label="Example Progress Bar"
-   aria-valuemin="0"
-   aria-valuemax="1"
->
+<div bind:this={divEle} class="mdc-circular-progress" style={sizes[size].style} role="progressbar" aria-label="Progress Bar" aria-valuemin="0" aria-valuemax="1">
    <div class="mdc-circular-progress__determinate-container">
-      <svg class="mdc-circular-progress__determinate-circle-graphic" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
-         <circle class="mdc-circular-progress__determinate-track" cx="24" cy="24" r="18" stroke-width="4" />
-         <circle class="mdc-circular-progress__determinate-circle" cx="24" cy="24" r="18" stroke-dasharray="113.097" stroke-dashoffset="113.097" stroke-width="4" />
+      <svg class="mdc-circular-progress__determinate-circle-graphic" viewBox={sizes[size].viewBox} xmlns="http://www.w3.org/2000/svg">
+         <circle class="mdc-circular-progress__determinate-track" cx={sizes[size].cx} cy={sizes[size].cy} r={sizes[size].r} stroke-width={sizes[size].width1} />
+         <circle
+            class="mdc-circular-progress__determinate-circle"
+            cx={sizes[size].cx}
+            cy={sizes[size].cy}
+            r={sizes[size].r}
+            stroke-dasharray={sizes[size].dash1}
+            stroke-dashoffset={sizes[size].dash1}
+            stroke-width={sizes[size].width1}
+         />
       </svg>
    </div>
    <div class="mdc-circular-progress__indeterminate-container">
       <div class="mdc-circular-progress__spinner-layer">
          <div class="mdc-circular-progress__circle-clipper mdc-circular-progress__circle-left">
-            <svg class="mdc-circular-progress__indeterminate-circle-graphic" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
-               <circle cx="24" cy="24" r="18" stroke-dasharray="113.097" stroke-dashoffset="56.549" stroke-width="4" />
+            <svg class="mdc-circular-progress__indeterminate-circle-graphic" viewBox={sizes[size].viewBox} xmlns="http://www.w3.org/2000/svg">
+               <circle
+                  cx={sizes[size].cx}
+                  cy={sizes[size].cy}
+                  r={sizes[size].r}
+                  stroke-dasharray={sizes[size].dash1}
+                  stroke-dashoffset={sizes[size].dash2}
+                  stroke-width={sizes[size].width1}
+               />
             </svg>
          </div>
          <div class="mdc-circular-progress__gap-patch">
-            <svg class="mdc-circular-progress__indeterminate-circle-graphic" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
-               <circle cx="24" cy="24" r="18" stroke-dasharray="113.097" stroke-dashoffset="56.549" stroke-width="3.2" />
+            <svg class="mdc-circular-progress__indeterminate-circle-graphic" viewBox={sizes[size].viewBox} xmlns="http://www.w3.org/2000/svg">
+               <circle
+                  cx={sizes[size].cx}
+                  cy={sizes[size].cy}
+                  r={sizes[size].r}
+                  stroke-dasharray={sizes[size].dash1}
+                  stroke-dashoffset={sizes[size].dash2}
+                  stroke-width={sizes[size].width2}
+               />
             </svg>
          </div>
          <div class="mdc-circular-progress__circle-clipper mdc-circular-progress__circle-right">
-            <svg class="mdc-circular-progress__indeterminate-circle-graphic" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
-               <circle cx="24" cy="24" r="18" stroke-dasharray="113.097" stroke-dashoffset="56.549" stroke-width="4" />
-            </svg>
-         </div>
-      </div>
-   </div>
-</div>
-
-<div
-   bind:this={divEleMedium}
-   class="mdc-circular-progress"
-   style="width:36px;height:36px;"
-   role="progressbar"
-   aria-label="Example Progress Bar"
-   aria-valuemin="0"
-   aria-valuemax="1"
->
-   <div class="mdc-circular-progress__determinate-container">
-      <svg class="mdc-circular-progress__determinate-circle-graphic" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-         <circle class="mdc-circular-progress__determinate-track" cx="16" cy="16" r="12.5" stroke-width="3" />
-         <circle class="mdc-circular-progress__determinate-circle" cx="16" cy="16" r="12.5" stroke-dasharray="78.54" stroke-dashoffset="78.54" stroke-width="3" />
-      </svg>
-   </div>
-   <div class="mdc-circular-progress__indeterminate-container">
-      <div class="mdc-circular-progress__spinner-layer">
-         <div class="mdc-circular-progress__circle-clipper mdc-circular-progress__circle-left">
-            <svg class="mdc-circular-progress__indeterminate-circle-graphic" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-               <circle cx="16" cy="16" r="12.5" stroke-dasharray="78.54" stroke-dashoffset="39.27" stroke-width="3" />
-            </svg>
-         </div>
-         <div class="mdc-circular-progress__gap-patch">
-            <svg class="mdc-circular-progress__indeterminate-circle-graphic" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-               <circle cx="16" cy="16" r="12.5" stroke-dasharray="78.54" stroke-dashoffset="39.27" stroke-width="2.4" />
-            </svg>
-         </div>
-         <div class="mdc-circular-progress__circle-clipper mdc-circular-progress__circle-right">
-            <svg class="mdc-circular-progress__indeterminate-circle-graphic" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-               <circle cx="16" cy="16" r="12.5" stroke-dasharray="78.54" stroke-dashoffset="39.27" stroke-width="3" />
-            </svg>
-         </div>
-      </div>
-   </div>
-</div>
-
-<div
-   bind:this={divEleSmall}
-   class="mdc-circular-progress"
-   style="width:24px;height:24px;"
-   role="progressbar"
-   aria-label="Example Progress Bar"
-   aria-valuemin="0"
-   aria-valuemax="1"
->
-   <div class="mdc-circular-progress__determinate-container">
-      <svg class="mdc-circular-progress__determinate-circle-graphic" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-         <circle class="mdc-circular-progress__determinate-track" cx="12" cy="12" r="8.75" stroke-width="2.5" />
-         <circle class="mdc-circular-progress__determinate-circle" cx="12" cy="12" r="8.75" stroke-dasharray="54.978" stroke-dashoffset="54.978" stroke-width="2.5" />
-      </svg>
-   </div>
-   <div class="mdc-circular-progress__indeterminate-container">
-      <div class="mdc-circular-progress__spinner-layer">
-         <div class="mdc-circular-progress__circle-clipper mdc-circular-progress__circle-left">
-            <svg class="mdc-circular-progress__indeterminate-circle-graphic" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-               <circle cx="12" cy="12" r="8.75" stroke-dasharray="54.978" stroke-dashoffset="27.489" stroke-width="2.5" />
-            </svg>
-         </div>
-         <div class="mdc-circular-progress__gap-patch">
-            <svg class="mdc-circular-progress__indeterminate-circle-graphic" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-               <circle cx="12" cy="12" r="8.75" stroke-dasharray="54.978" stroke-dashoffset="27.489" stroke-width="2" />
-            </svg>
-         </div>
-         <div class="mdc-circular-progress__circle-clipper mdc-circular-progress__circle-right">
-            <svg class="mdc-circular-progress__indeterminate-circle-graphic" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-               <circle cx="12" cy="12" r="8.75" stroke-dasharray="54.978" stroke-dashoffset="27.489" stroke-width="2.5" />
+            <svg class="mdc-circular-progress__indeterminate-circle-graphic" viewBox={sizes[size].viewBox} xmlns="http://www.w3.org/2000/svg">
+               <circle
+                  cx={sizes[size].cx}
+                  cy={sizes[size].cy}
+                  r={sizes[size].r}
+                  stroke-dasharray={sizes[size].dash1}
+                  stroke-dashoffset={sizes[size].dash2}
+                  stroke-width={sizes[size].width1}
+               />
             </svg>
          </div>
       </div>
@@ -158,7 +131,7 @@
 <style lang="scss" global>
    @use './src/scss/theme' as vantage;
    @use '@material/theme' with (
-      $primary: vantage.$secondary,
+      $primary: vantage.$primary,
       $secondary: vantage.$secondary
    );
 
