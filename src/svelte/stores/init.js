1f7b07a472d2eb3f966e16c63e7520696756ff97
Gregory Harder <gharder@hwec.com>
2022-05-31 11:26:42 -0500

    Patch for chromium browsers

--- src/svelte/stores/init.js
+++ src/svelte/stores/init.js
@@ -6,8 +6,16 @@
 // Imports
 import { writable } from 'svelte/store';
 
+let mobile;
+
+if (navigator.userAgentData) {
+   mobile = navigator.userAgentData.mobile;
+} else {
+   mobile = /Mobi|Android/i.test(navigator.userAgent);
+}
+
 // Creates the custom store and sets up renewal loop
-const { set, subscribe, update } = writable({ serviceWorker: false, syncManager: false, mobile: /Mobi|Android/i.test(navigator.userAgent) });
+const { set, subscribe, update } = writable({ serviceWorker: false, syncManager: false, mobile });
 
 // Export the store object
 export default {
