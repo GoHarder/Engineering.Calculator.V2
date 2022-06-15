78a89c1b4ccd1a0e7efd28a8669f6b3f9592a28e
Gregory Harder <gharder@hwec.com>
2022-06-15 15:30:52 -0500

    Applied MongoDB recomendations

--- server/data/mongodb/pipelines/buffers.mjs
+++ server/data/mongodb/pipelines/buffers.mjs
@@ -4,6 +4,26 @@
 
 import { average, round } from '../../../../lib/math.mjs';
 
+// export const getOilBuffers = (load, speed) => {
+//    return [
+//       { $unwind: { path: '$applications' } },
+//       { $match: { maxSpeed: { $gte: speed }, $and: [{ 'applications.minLoad': { $lte: load } }, { 'applications.maxLoad': { $gte: load } }] } },
+//       { $sort: { name: 1, 'applications.qty': 1 } },
+//       {
+//          $group: {
+//             _id: '$_id',
+//             name: { $first: '$name' },
+//             stroke: { $first: '$stroke' },
+//             height: { $first: '$height' },
+//             applications: { $push: '$applications' },
+//             minQty: { $first: '$applications.qty' },
+//             maxQty: { $last: '$applications.qty' },
+//          },
+//       },
+//       { $sort: { name: 1 } },
+//    ];
+// };
+
 /**
  * Creates an aggregation for oil buffers
  * @param {number} load The load on the buffers
@@ -11,23 +31,34 @@ import { average, round } from '../../../../lib/math.mjs';
  */
 export const getOilBuffers = (load, speed) => {
    return [
-      { $unwind: { path: '$applications' } },
-      { $match: { maxSpeed: { $gte: speed }, $and: [{ 'applications.minLoad': { $lte: load } }, { 'applications.maxLoad': { $gte: load } }] } },
-      { $sort: { name: 1, 'applications.qty': 1 } },
-      {
-         $group: {
-            _id: '$_id',
-            name: { $first: '$name' },
-            stroke: { $first: '$stroke' },
-            height: { $first: '$height' },
-            applications: { $push: '$applications' },
-            minQty: { $first: '$applications.qty' },
-            maxQty: { $last: '$applications.qty' },
-         },
-      },
+      { $match: { maxSpeed: { $gte: speed }, applications: { $elemMatch: { minLoad: { $lte: load }, maxLoad: { $gte: load } } } } },
+      { $set: { applications: { $filter: { input: '$applications', cond: { $and: [{ $lte: ['$$this.minLoad', load] }, { $gte: ['$$this.maxLoad', load] }] } } } } },
+      { $set: { minQty: { $first: '$applications.qty' }, maxQty: { $last: '$applications.qty' } } },
       { $sort: { name: 1 } },
    ];
 };
+
+// export const getBufferSprings = (minLoad, maxLoad, speed) => {
+//    return [
+//       { $unwind: { path: '$applications' } },
+//       { $match: { 'applications.maxSpeed': { $gte: speed }, $and: [{ 'applications.load': { $gte: minLoad } }, { 'applications.load': { $lte: maxLoad } }] } },
+//       { $addFields: { difference: { $abs: { $subtract: [round(average([minLoad, maxLoad])), '$applications.load'] } } } },
+//       { $sort: { difference: 1, 'applications.qty': 1, 'applications.compression': -1 } },
+//       {
+//          $group: {
+//             _id: '$_id',
+//             name: { $first: '$name' },
+//             height: { $first: '$height' },
+//             loadPerInch: { $first: '$loadPerInch' },
+//             applications: { $push: '$applications' },
+//             calculated: { $first: '$applications' },
+//             compression: { $addToSet: '$applications.compression' },
+//          },
+//       },
+//       { $project: { 'applications.maxSpeed': 0, 'calculated.maxSpeed': 0 } },
+//       { $sort: { name: 1 } },
+//    ];
+// };
 
 /**
  * Creates an aggregation for buffer springs
@@ -37,9 +68,28 @@ export const getOilBuffers = (load, speed) => {
  */
 export const getBufferSprings = (minLoad, maxLoad, speed) => {
    return [
-      { $unwind: { path: '$applications' } },
       { $match: { 'applications.maxSpeed': { $gte: speed }, $and: [{ 'applications.load': { $gte: minLoad } }, { 'applications.load': { $lte: maxLoad } }] } },
-      { $addFields: { difference: { $abs: { $subtract: [round(average([minLoad, maxLoad])), '$applications.load'] } } } },
+      {
+         $set: {
+            applications: {
+               $filter: {
+                  input: '$applications',
+                  cond: { $and: [{ $gte: ['$$this.maxSpeed', speed] }, { $gte: ['$$this.load', minLoad] }, { $lte: ['$$this.load', maxLoad] }] },
+               },
+            },
+         },
+      },
+      {
+         $set: {
+            applications: {
+               $map: {
+                  input: '$applications',
+                  in: { $mergeObjects: ['$$this', { difference: { $abs: { $subtract: [round(average([minLoad, maxLoad])), '$$this.load'] } } }] },
+               },
+            },
+         },
+      },
+      { $unwind: '$applications' },
       { $sort: { difference: 1, 'applications.qty': 1, 'applications.compression': -1 } },
       {
          $group: {
@@ -52,7 +102,6 @@ export const getBufferSprings = (minLoad, maxLoad, speed) => {
             compression: { $addToSet: '$applications.compression' },
          },
       },
-      { $project: { 'applications.maxSpeed': 0, 'calculated.maxSpeed': 0 } },
       { $sort: { name: 1 } },
    ];
 };
